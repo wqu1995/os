9ca3f9a12a89e46143415357f35323d1ada59378
wqu1995 <wqu1995@gmail.com>
2019-04-23 13:01:28 -0400

    final product

--- lab3/diff.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for(( i = 1; i<=11; i++))
-do
-	for((j = 1; j<=128; j++))
-	do
-		echo $i $j
-		diff -b -B -E <(./test  in$i -oOPFS -f $j rfile -a w) <(./mmu -a w -o OPFS -f $j in$i rfile)
-	done
-done
